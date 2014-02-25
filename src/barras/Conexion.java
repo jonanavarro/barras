7411caef9d8526c275dc9e48c64a5be84942295e
efra1323 <efra1323@pUnK1323-lap>
2014-02-25 11:18:18 -0600

    modifique la modificacion y la hice mas vergas

--- src/barras/Conexion.java
+++ src/barras/Conexion.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-/*MODIFICACION CHIDA 123456*/
+/*MODIFICACION bien GAVER*/
 package barras;
 import java.sql.*;
 
