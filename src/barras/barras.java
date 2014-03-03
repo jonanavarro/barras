6d399a4cb21e2e5a6d58b5c5fd54992eb43ff02d
Jonathan <Jonathan@Jonathan-PC.lan>
2014-03-03 15:57:19 -0600

    Splash screen

--- src/barras/barras.java
+++ src/barras/barras.java
@@ -1,5 +1,8 @@
 package barras;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -15,6 +18,11 @@ public class barras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(barras.class.getName()).log(Level.SEVERE, null, ex);
+        }
         Conexion con = new Conexion();
         con.establecerConexion();
         new principal().setVisible(true);
